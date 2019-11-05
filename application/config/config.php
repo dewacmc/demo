12e2d0ca3db7196ae1e523e9b5f7b4e530871e49
bapakdewa <45229703+picturedewa@users.noreply.github.com>
2019-11-06 01:01:01 +0700

    db

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "http://3aproduction.com/posapi/";
+$config['base_url'] = "http://democmclearningcom-via.cloud.revoluz.io/";
 
 /*
 |--------------------------------------------------------------------------
