11b15321ebd63922d1644756666ce5794e031e26
bapakdewa <45229703+picturedewa@users.noreply.github.com>
2020-01-04 00:04:34 +0700

    Update Jadwalmodel.php

--- application/models/Jadwalmodel.php
+++ application/models/Jadwalmodel.php
@@ -32,7 +32,7 @@ class JadwalModel extends CI_Model
     return $query->result();
   }
 
-  public function getAllterapis($terapis){
+  public function getAllterapis_lama($terapis){
     $curMonth = date('F');
     $curYear  = date('Y');
     $timestamp    = strtotime($curMonth.' '.$curYear);
@@ -50,6 +50,26 @@ class JadwalModel extends CI_Model
     $this->db->where('A.start BETWEEN "'. date('Y-m-d', strtotime($first)). '" and "'. date('Y-m-d', strtotime($last)).'"');
     $query = $this->db->get();
     return $query->result();
+  }
+
+
+  public function getAllterapis($terapis){
+    $curMonth = date('F');
+    $curYear  = date('Y');
+    $timestamp    = strtotime($curMonth.' '.$curYear);
+    $first = date('Y-m-01 00:00:00', $timestamp);
+    $last  = date('Y-m-t 12:59:59', $timestamp); 
+    $this->db->select('A.id, CONCAT(G.nama,": ",D.name,"- ",E.nama ) AS title,A.start, A.end, E.nama as cabang, G.nama as terapis, D.name as room, D.color');
+    $this->db->from("{$this->jadwal_h} A");
+    $this->db->join("{$this->jadwal_d1} C", 'A.id = C.id');
+    $this->db->join("{$this->room} D", 'A.idroom = D.idroom');
+    $this->db->join("{$this->cabang} E", 'A.idcab = E.id');
+    $this->db->join("{$this->terapis} G", 'C.idterapis = G.id');
+    $this->db->where('G.googleid', $terapis);
+    $this->db->where('A.start BETWEEN "'. date('Y-m-d', strtotime($first)). '" and "'. date('Y-m-d', strtotime($last)).'"');
+    $query = $this->db->get();
+    return $query->result();
+
   }
 
   public function getFiter($terapis,$cabang,$room){
