ecf797e0ed52ced91f8396f022bef1d2ec790a57
bapakdewa <45229703+picturedewa@users.noreply.github.com>
2020-01-03 19:05:59 +0700

    Update Hasilmodel.php

--- application/models/Hasilmodel.php
+++ application/models/Hasilmodel.php
@@ -10,6 +10,8 @@ class HasilModel extends CI_Model
   public $jadwal_h= 'cmc_jadwal_h';
   public $jadwal_d= 'cmc_jadwal_d';
   public $jadwal_d1= 'cmc_jadwal_d1';
+  public $order= 'order_siswa';
+  public $product= 'prod';
 
   public function __construct()
 
@@ -24,7 +26,7 @@ class HasilModel extends CI_Model
     // $timestamp    = strtotime($curMonth.' '.$curYear);
     // $first = date('Y-m-01 00:00:00', $timestamp);
     // $last  = date('Y-m-t 12:59:59', $timestamp); 
-    $this->db->select('A.id, CONCAT(G.nama,": ",F.nama,"- " ,D.name,"- ",E.nama ) AS title,A.start, A.end, A.jenis, E.nama as cabang, G.nama as terapis, D.name as room, D.color, F.tgldaftar, F.nama');
+    $this->db->select('A.id, CONCAT(G.nama,": ",F.nama,"- " ,D.name,"- ",E.nama ) AS title,A.start, B.idord, A.end, A.jenis, E.nama as cabang, G.nama as terapis, D.name as room, D.color, F.tgldaftar, F.nama');
     $this->db->from("{$this->jadwal_h} A");
     $this->db->join("{$this->jadwal_d} B", 'A.id = B.id');
     $this->db->join("{$this->jadwal_d1} C", 'A.id = C.id');
