ebed0469c4b9bc7ffa25cef49aa9aa448cd5f904
bapakdewa <45229703+picturedewa@users.noreply.github.com>
2020-01-03 19:12:26 +0700

    Update Hasilmodel.php

--- application/models/Hasilmodel.php
+++ application/models/Hasilmodel.php
@@ -26,7 +26,7 @@ class HasilModel extends CI_Model
     // $timestamp    = strtotime($curMonth.' '.$curYear);
     // $first = date('Y-m-01 00:00:00', $timestamp);
     // $last  = date('Y-m-t 12:59:59', $timestamp); 
-    $this->db->select('A.id, CONCAT(G.nama,": ",F.nama,"- " ,D.name,"- ",E.nama ) AS title,A.start, B.idord, A.end, A.jenis, E.nama as cabang, G.nama as terapis, D.name as room, D.color, F.tgldaftar, F.nama');
+    $this->db->select('A.id, CONCAT(G.nama,": ",F.nama,"- " ,D.name,"- ",E.nama ) AS title,A.start, B.idord, I.nama, A.end, A.jenis, E.nama as cabang, G.nama as terapis, D.name as room, D.color, F.tgldaftar, F.nama');
     $this->db->from("{$this->jadwal_h} A");
     $this->db->join("{$this->jadwal_d} B", 'A.id = B.id');
     $this->db->join("{$this->jadwal_d1} C", 'A.id = C.id');
@@ -34,6 +34,8 @@ class HasilModel extends CI_Model
     $this->db->join("{$this->cabang} E", 'A.idcab = E.id');
     $this->db->join("{$this->client} F", 'B.iduser = F.idpasien');
     $this->db->join("{$this->terapis} G", 'C.idterapis = G.id');
+    $this->db->join("{$this->order} H", 'B.idord = H.idord');
+    $this->db->join("{$this->product} I", 'H.idprod = I.idprod');
     $this->db->where('A.id', $idjadwal);
     // $this->db->where('A.start BETWEEN "'. date('Y-m-d', strtotime($first)). '" and "'. date('Y-m-d', strtotime($last)).'"');
     $query = $this->db->get();
