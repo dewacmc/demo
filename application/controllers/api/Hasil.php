36a23ae80697eb71181389770e0fcbf868664342
bapakdewa <45229703+picturedewa@users.noreply.github.com>
2020-01-03 17:55:47 +0700

    hasil

---
+++ application/controllers/api/Hasil.php
@@ -0,0 +1,34 @@
+<?php
+//use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+//To Solve File REST_Controller not found
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Hasil extends REST_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Hasilmodel","MHasil");
+       
+    }
+
+    public function getalldata(){
+            $idtrp = $this->post("idjadwal");
+            $jadwalData = $this->Mjadwal->getAlldata($idtrp);
+            if (count($jadwalData) == 0) {
+                $jadwal["message"] = "Id Jadwal Tidak ditemukan";
+                $jadwal["success"] = 0;
+            }else{
+                $jadwal["success"] = 1;
+                $jadwal["message"] = "success show list all Data";
+            }
+               
+            $jadwal["data"] = $jadwalData;
+            $this->response($jadwal, REST_Controller::HTTP_OK);
+    }
+}
---
+++ application/models/Hasilmodel.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class HasilModel extends CI_Model
+{
+  public $room = 'cmc_room';
+  public $cabang = 'cmc_cabang';
+  public $terapis='cmc_terapis';
+  public $client= 'cmc_client';
+  public $jadwal_h= 'cmc_jadwal_h';
+  public $jadwal_d= 'cmc_jadwal_d';
+  public $jadwal_d1= 'cmc_jadwal_d1';
+
+  public function __construct()
+    public $room = 'cmc_room';
+    public $cabang = 'cmc_cabang';
+    public $terapis='cmc_terapis';
+    public $client= 'cmc_client';
+    public $jadwal_h= 'cmc_jadwal_h';
+    public $jadwal_d= 'cmc_jadwal_d';
+    public $jadwal_d1= 'cmc_jadwal_d1';
+  {
+    parent::__construct();
+    $this->load->database();
+  }
+
+  public function getAlldata($idjadwal){
+    // $curMonth = date('F');
+    // $curYear  = date('Y');
+    // $timestamp    = strtotime($curMonth.' '.$curYear);
+    // $first = date('Y-m-01 00:00:00', $timestamp);
+    // $last  = date('Y-m-t 12:59:59', $timestamp); 
+    $this->db->select('A.id, CONCAT(G.nama,": ",F.nama,"- " ,D.name,"- ",E.nama ) AS title,A.start, A.end, A.jenis, E.nama as cabang, G.nama as terapis, D.name as room, D.color, F.tgldaftar, F.nama');
+    $this->db->from("{$this->jadwal_h} A");
+    $this->db->join("{$this->jadwal_d} B", 'A.id = B.id');
+    $this->db->join("{$this->jadwal_d1} C", 'A.id = C.id');
+    $this->db->join("{$this->room} D", 'A.idroom = D.idroom');
+    $this->db->join("{$this->cabang} E", 'A.idcab = E.id');
+    $this->db->join("{$this->client} F", 'B.iduser = F.idpasien');
+    $this->db->join("{$this->terapis} G", 'C.idterapis = G.id');
+    $this->db->where('A.id', $idjadwal);
+    // $this->db->where('A.start BETWEEN "'. date('Y-m-d', strtotime($first)). '" and "'. date('Y-m-d', strtotime($last)).'"');
+    $query = $this->db->get();
+    return $query->result();
+  }
+}
