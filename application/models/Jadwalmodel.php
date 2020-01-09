32d49600b02be6f721be79facfcc1c012eba65b7
bapakdewa <45229703+picturedewa@users.noreply.github.com>
2020-01-09 07:59:33 +0700

    Update Jadwalmodel.php

--- application/models/Jadwalmodel.php
+++ application/models/Jadwalmodel.php
@@ -143,7 +143,7 @@ class JadwalModel extends CI_Model
   public function getFiterdate($terapis,$tglcari){
     $time = strtotime($tglcari);
     $datecari = date('Y-m-d', $time);
-    $this->db->select('A.id, A.start, A.end, E.nama as cabang, G.nama as terapis, D.name as room, D.color');
+    $this->db->select('A.id, A.start, A.end, E.nama as cabang, G.nama as terapis, D.name as room, D.colors');
     $this->db->from("{$this->jadwal_h} A");
     $this->db->join("{$this->jadwal_d1} C", 'A.id = C.id');
     $this->db->join("{$this->room} D", 'A.idroom = D.idroom');
